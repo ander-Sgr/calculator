714d0ca33926dc4aa6ace3d0a08a7277dcc9dac1
Anderson Estrella <andersonbarreto2001@gmail.com>
2022-07-18 23:17:39 +0200

    refactoring code

--- scripts/main.js
+++ scripts/main.js
@@ -1,76 +1,92 @@
-
-const clearButton = document.getElementById('clear-button');
-const plusMinBtn = document.getElementById('plus-minus-button');
-const commaButton = document.getElementById('comma-button');
-const operatorButtons = document.getElementsByClassName('operator-button');
-
-let firstNumber = "";
+let currentValue = "0";
 let secondNumber = "";
 let operator = "";
+let num1 = 0;
+const numbers = document.querySelectorAll('button');
+const resultScreen = document.getElementById('result-label');
+const operators = document.querySelectorAll('.operator-button');
 
-const resultScreen = document.querySelector("#result-label");
-const numbersButtons = document.querySelectorAll(".number-button");
-numbersButtons.forEach(buttonNumber => {
-    buttonNumber.addEventListener('click', (event) => {
-        let newInput = event.target.textContent;
-        console.log(newInput);
-         inputComma(buttonNumber.textContent);
-         firstNumber = inputNumber(buttonNumber.textContent);    
-    });
+function updateDisplay() {
+    resultScreen.textContent = currentValue;
 
-});
+}
 
-console.log(firstNumber);
+function numbersButtons() {
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener('click', function () {
+            if (numbers[i].classList.contains('number-button') && numbers[i].value !== ',') {
+                inputNumber(numbers[i].value);
 
-Array.from(operatorButtons).forEach(op => {
-    op.addEventListener('click', () => {
-        inputOperator(op.textContent);
-        inputPlusMinus(op.textContent);
-    })
-});
+            } else if (numbers[i].value === ',') {
+                inputComma(numbers[i].value);
+                // updateDisplay();
+            }
+            updateDisplay();
+        })
+    }
+}
+
+function operatorsButtons() {
+    for (let i = 0; i < operators.length; i++) {
+        operators[i].addEventListener('click', function () {
+            if (operators[i].value === 'clear') {
+                clearButton();
+            } else if (operators[i].value === 'plusMin') {
+                inputPlusMinus(currentValue);
+            }
+            updateDisplay();
+        });
+
+    }
+}
 
 function inputNumber(digit) {
-    if (resultScreen.textContent === '0' && digit !== ',') {
-        return resultScreen.textContent = digit;
+
+    if (currentValue === '0') {
+        currentValue = digit;
     } else {
-        return resultScreen.textContent += digit;
+        currentValue += digit;
     }
-
+    //resultScreen.textContent = currentValue;
 }
-
 function inputComma(digit) {
-    if (digit === ',') {
-        commaButton.disabled = true;
+    if (currentValue === '0' || currentValue === secondNumber) {
+        currentValue += digit;
+    } else if (!resultScreen.textContent.includes(digit)) {
+        currentValue += digit;
+    }
+}
+
+function inputPlusMinus(number) {
+    let replaceComma;
+    if (number.includes(',')) {
+        replaceComma = number.replace(',', '.');
+        replaceComma *= -1;
+        currentValue = replaceComma.toString().replace('.', ',');
+    } else {
+        currentValue = (number * -1).toString();
+    }
+}
+
+function clearButton() {
+    currentValue = "0";
+    secondNumber = ""
+    operator = "";
+    resultScreen.textContent = currentValue;
+}
+
+function inputOperator(operatorButton){
+    if(operator === ''){
+        
     }
 }
 
 
-function inputPlusMinus(btnOperator) {
-    let existsSign = resultScreen.textContent.includes('-');
 
-    if ((!existsSign && resultScreen.textContent !== '0' && btnOperator === '+/-')) {
-        if (resultScreen.textContent !== '0,') {
-            resultScreen.textContent = '-'.concat(resultScreen.textContent);
-        }
-    } else if (btnOperator === '+/-') {
-        resultScreen.textContent = resultScreen.textContent.replace('-', '');
-    }
+function run(){
+    numbersButtons();
+    updateDisplay();
+    operatorsButtons();
 }
 
-function inputOperator(btnOperator) {
-
-    switch (btnOperator) {
-        case 'C':
-            resultScreen.textContent = '0';
-            commaButton.disabled = false;
-            firstNumber = "";
-            secondNumber = "";
-            break;
-        case '+':
-            commaButton.disabled = false;
-            operator = btnOperator;
-            break;
-        default:
-            break;
-    }
-}
+run();
