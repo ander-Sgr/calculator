0a46c51ceda285c09a22ba4b273618d468929520
Anderson Estrella <andersonbarreto2001@gmail.com>
2022-07-20 12:36:06 +0200

    refacotring for the inpùt 2do number

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,14 @@
-let currentValue = "0";
+let displayValue = "0";
+let firstNumber = "";
 let secondNumber = "";
 let operator = "";
-let num1 = 0;
+
 const numbers = document.querySelectorAll('button');
 const resultScreen = document.getElementById('result-label');
 const operators = document.querySelectorAll('.operator-button');
 
 function updateDisplay() {
-    resultScreen.textContent = currentValue;
+    resultScreen.textContent = displayValue;
 
 }
 
@@ -16,10 +17,8 @@ function numbersButtons() {
         numbers[i].addEventListener('click', function () {
             if (numbers[i].classList.contains('number-button') && numbers[i].value !== ',') {
                 inputNumber(numbers[i].value);
-
             } else if (numbers[i].value === ',') {
                 inputComma(numbers[i].value);
-                // updateDisplay();
             }
             updateDisplay();
         })
@@ -31,62 +30,134 @@ function operatorsButtons() {
         operators[i].addEventListener('click', function () {
             if (operators[i].value === 'clear') {
                 clearButton();
+
             } else if (operators[i].value === 'plusMin') {
-                inputPlusMinus(currentValue);
+                inputPlusMinus(displayValue);
+            } else {
+                inputOperator(operators[i].value);
             }
+            //  inputOperator(operators[i].value);
             updateDisplay();
         });
 
     }
 }
 
-function inputNumber(digit) {
 
-    if (currentValue === '0') {
-        currentValue = digit;
+
+
+function inputNumber(digit) {
+    if (operator === '') {
+        if (displayValue === '0') {
+            displayValue = digit;
+        } else {
+            displayValue += digit;
+        }
     } else {
-        currentValue += digit;
+        //second input 
+        if (displayValue === firstNumber) {
+            displayValue = digit;
+        } else {
+            displayValue += digit;
+        }
     }
-    //resultScreen.textContent = currentValue;
+
 }
 function inputComma(digit) {
-    if (currentValue === '0' || currentValue === secondNumber) {
-        currentValue += digit;
+    if (displayValue === '0' || displayValue === secondNumber) {
+        displayValue += digit;
     } else if (!resultScreen.textContent.includes(digit)) {
-        currentValue += digit;
+        displayValue += digit;
     }
 }
 
 function inputPlusMinus(number) {
     let replaceComma;
-    if (number.includes(',')) {
+    if (resultScreen.textContent.includes(',')) {
         replaceComma = number.replace(',', '.');
-        replaceComma *= -1;
-        currentValue = replaceComma.toString().replace('.', ',');
+        replaceComma = replaceComma * -1;
+        displayValue = replaceComma.toString().replace('.', ',');
     } else {
-        currentValue = (number * -1).toString();
+        displayValue = (number * -1).toString();
     }
 }
 
 function clearButton() {
-    currentValue = "0";
+    displayValue = "0";
     secondNumber = ""
+    highLightOperator('');
+
     operator = "";
-    resultScreen.textContent = currentValue;
+
+    // resultScreen.textContent = 0;
 }
 
-function inputOperator(operatorButton){
-    if(operator === ''){
-        
+function inputOperator(operatorButton) {
+    if (operator !== '') {
+        secondNumber = displayValue;
+        result = performingOperation(parseFloat(convertDecimal(firstNumber)), parseFloat(convertDecimal(secondNumber)), operator);
+        displayValue = result;
+        firstNumber = displayValue;
+        result = '';
+
+    } else {
+        operator = operatorButton;
+        firstNumber = displayValue;
+        highLightOperator(operatorButton)
     }
+    highLightOperator('')
+}
+
+function performingOperation(num1, num2, opButton) {
+    let result;
+    switch (opButton) {
+        case 'plus':
+            result = num1 + num2;
+
+            break;
+        case 'minus':
+            result = num1 - num2;
+
+            break;
+        case 'multi':
+            result = num1 * num2;
+
+            break;
+        case 'divide':
+            if (num2 === 0) {
+                return 'ERROR';
+            }
+            result = num1 / num2;
+
+            break;
+        default:
+            break;
+    }
+
+    return result;
+}
+
+function highLightOperator(operator) {
+    for (let i = 0; i < operators.length; i++) {
+        if (operator !== '') {
+            operators[i].style.backgroundColor = 'red';
+        }
+      //  operators[i].style.backgroundColor = 'blue';
+
+    }
+
 }
 
 
 
-function run(){
+function convertDecimal(number) {
+    return parseFloat(number.replace(',', '.'));
+}
+
+function run() {
     numbersButtons();
-    updateDisplay();
     operatorsButtons();
+    updateDisplay();
 }
 
 run();
