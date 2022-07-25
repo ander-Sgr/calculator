c7a72902fade9564ef3b356be5b828aaa34e1b04
Anderson Estrella <andersonbarreto2001@gmail.com>
2022-07-25 11:31:00 +0200

    commit all files

--- scripts/main.js
+++ scripts/main.js
@@ -1,171 +1,228 @@
-let displayValue = "0";
-let firstNumber = "";
-let secondNumber = "";
-let operator = "";
-
-const resultScreen = document.getElementById('result-label');
-const numbers = document.querySelectorAll('button');
+const displayScreen = document.getElementById('result-label');
+const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator-button');
-const clearButton = document.getElementById('clear-button').value;
-const plusMinButton = document.getElementById('plus-minus-button').value;
+const plusMinus = document.getElementById('plus-minus-button').textContent;
 
-function updateDisplay() {
-    resultScreen.textContent = displayValue;
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+let existsPreviusNumber = false;
+let textInScreen = getDisplayResult();
+let arrOperator = ['+', '-', '/', 'x', '*', '='];
+/****events****/
+
+window.addEventListener("keydown", inputKeys);
+
+function inputKeys(e) {
+    e.preventDefault();
+    if (e.key >= 0 && e.key <= 9) {
+        inputNumber(e.key);
+    } else if (e.key === 'Escape') {
+        resetCalculator();
+    } else if (e.key === ',') {
+        inputComma(e.key);
+    } else if (e.key === 'Control') {
+        inputPlusMinus(displayScreen.textContent);
+    } else {
+        arrOperator.forEach(iterator => {
+            if (iterator === e.key) {
+                inputOperator(e.key);
+
+                console.log(e.key, ' ', iterator)
+            }
+        });
+    }
 }
 
-operators.forEach((opBtn) => {
-    opBtn.addEventListener('click', (elEvent) => {
-        let operatorPress = elEvent.target.value;
-        switch (operatorPress) {
-            case plusMinButton:
-                inputPlusMinus(displayValue);
-                break;
-            case clearButton:
-                resetCalculator();
-                break;
-            default:
-                break;
-        }
-        if (operatorPress !== plusMinButton) {
-            inputOperator(operatorPress);
-        }
-
-
-
-        console.log(operatorPress);
-    });
-});
-
-function numbersButtons() {
+function getNumbers() {
     for (let i = 0; i < numbers.length; i++) {
         numbers[i].addEventListener('click', function () {
-            if (numbers[i].classList.contains('number-button') && numbers[i].value !== ',') {
-                inputNumber(numbers[i].value);
-            } else if (numbers[i].value === ',') {
-                inputComma(numbers[i].value);
+            let input = numbers[i].textContent;
+            if (input !== ',') {
+                inputNumber(numbers[i].textContent);
+            } else {
+                inputComma(input);
+
             }
-            updateDisplay();
-        })
+        });
     }
+}
+
+function getOperators() {
+    for (let i = 0; i < operators.length; i++) {
+        operators[i].addEventListener('click', function () {
+            let input = operators[i].textContent;
+            if (input === plusMinus) {
+                inputPlusMinus(displayScreen.textContent);
+            } else if (input === 'C') {
+                resetCalculator();
+            }
+            arrOperator.forEach(iterator => {
+                if (iterator === input) {
+                    inputOperator(input);
+                }
+            });
+            setDisplayResult(textInScreen);
+        });
+    }
+}
+
+getNumbers();
+getOperators();
+
+/** functions **/
+function getDisplayResult() {
+    return displayScreen.textContent;
+}
+
+function setDisplayResult(input) {
+    displayScreen.textContent = input;
 }
 
 function inputNumber(digit) {
-    if (operator === '') {
-        if (displayValue === '0') {
-            displayValue = digit;
-        } else {
-            if (displayValue.length < 10) {
-                displayValue += digit;
-            }
-        }
-    } else {
-        //second input 
-        if (displayValue === firstNumber) {
-            displayValue = digit;
-        } else {
-            if (displayValue.length < 10) {
-                displayValue += digit;
-            }
 
+    if (operator !== null && secondNumber === null) {
+        textInScreen = '';
+        textInScreen += digit;
+        secondNumber = digit;
+    } else {
+        if (textInScreen === '0') {
+            textInScreen = digit;
+        } else {
+            textInScreen += digit;
         }
     }
-
-
+    setDisplayResult(textInScreen);
 
 }
-function inputComma(digit) {
-    if (displayValue === '0' || displayValue === secondNumber) {
-        displayValue += digit;
-    } else if (!resultScreen.textContent.includes(digit)) {
-        displayValue += digit;
+
+function inputComma(comma) {
+    if (textInScreen === '0') {
+        textInScreen = '0'.concat(comma);
+    } else if (!textInScreen.includes(',')) {
+        textInScreen += comma;
     }
+    setDisplayResult(textInScreen);
+    highLightComma(comma);
+}
+
+function highLightComma(comma) {
+    comma = document.getElementById('comma-button').classList.add("higLightNumbers");
 }
 
 function inputPlusMinus(number) {
     let replaceComma;
-    if (displayValue[displayValue.length - 1] == ',') {
-        replaceComma = number.slice(0, displayValue.length - 1) * -1;
+    if (textInScreen[textInScreen.length - 1] == ',') {
+        replaceComma = number.slice(0, textInScreen.length - 1) * -1;
         replaceComma += ',';
-        displayValue = replaceComma;
+        textInScreen = replaceComma;
+    } else if (displayScreen.textContent.indexOf(',') !== -1) {
+        replaceComma = number.replace(',', '.');
+        replaceComma = replaceComma * -1;
+        textInScreen = replaceComma.toString().replace('.', ',');
     } else {
-        if (resultScreen.textContent.includes(',')) {
-            replaceComma = number.replace(',', '.');
-            replaceComma = replaceComma * -1;
-            displayValue = replaceComma.toString().replace('.', ',');
-        } else {
-            displayValue = (number * -1).toString();
+        textInScreen = (number * -1);
+    }
+    setDisplayResult(textInScreen);
+}
+
+function replaceComma(number) {
+    return number.replace(',', '.');
+}
+
+function replaceDot(number) {
+    let numberToString;
+    numberToString = number.toString();
+    if (typeof (number) === 'number') {
+        numberToString = numberToString.replace('.', ',');
+    }
+    numberToString = numberToString.replace('.', ',');
+    return numberToString;
+}
+
+function getFirstNumber(operatorBtn) {
+    operator = operatorBtn;
+    firstNumber = parseFloat(replaceComma(textInScreen));
+}
+
+function getSecondNumber(operatorBtn) {
+    operator = operatorBtn
+    secondNumber = parseFloat(replaceComma(textInScreen));
+
+}
+
+function inputOperator(operatorBtn) {
+    let result;
+    if (operator === null) {
+        getFirstNumber(operatorBtn);
+
+    } else if (operatorBtn === '=' && operator !== null) {
+        getSecondNumber(operator);
+        result = performingOperation(firstNumber, secondNumber, operator);
+        textInScreen = replaceDot(result);
+    }
+    operator = operatorBtn;
+
+    highlightOperator(operator);
+
+
+}
+
+function highlightOperator(operatorBtn) {
+    unHighLightOperator();
+    for (let i = 0; i < operators.length; i++) {
+        if (operators[i].textContent === operatorBtn) {
+            operators[i].classList.add("highLightOperator")
         }
-    }
-
-}
-
-
-function resetCalculator() {
-    displayValue = "0";
-    firstNumber = "";
-    secondNumber = ""
-    operator = "";
-
-    // resultScreen.textContent = 0;
-}
-
-function inputOperator(operatorButton) {
-    let result;
-    if (operator !== '') {
-        operator = operatorButton;
-        secondNumber = displayValue;
-        result = performingOperation(convertDecimal(firstNumber), convertDecimal(secondNumber), operatorButton);
-        displayValue = result;
-        console.log(result)
-
-        firstNumber = displayValue;
-        
 
     }
-    operator = operatorButton;
-    firstNumber = displayValue;
-    console.log('first', firstNumber, 'op', operator, 'second', secondNumber, 'result', result);
-
 }
 
-function performingOperation(num1, num2, opButton) {
+function unHighLightOperator() {
+    for (let i = 0; i < operators.length; i++) {
+        operators[i].classList.remove("highLightOperator");
+    }
+    document.getElementById('comma-button').classList.remove("higLightNumbers");
+}
+
+function performingOperation(num1, num2, operatorBtn) {
     let result;
-    switch (opButton) {
-        case 'plus':
+    switch (operatorBtn) {
+        case '+':
             result = num1 + num2;
-
             break;
-        case 'minus':
+        case '-':
             result = num1 - num2;
-
             break;
-        case 'multi':
+        case 'x':
+        case '*':
             result = num1 * num2;
             break;
-        case 'divide':
+        case '/':
             if (num2 === 0) {
-                return 'ERROR';
+                result = 'ERROR';
             }
             result = num1 / num2;
             break;
         default:
             break;
+
     }
+    console.log('firstnumber', firstNumber, 'oeprator', operator, 'second', secondNumber, ' result ', result)
 
     return result;
 }
 
-
-function convertDecimal(number) {
-    if(resultScreen.textContent.includes('.'))return;
-    return parseFloat(number.replace(',', '.'));
+function roundResult(result, places) {
+    return parseFloat(Math.round(result + 'e' + places) + 'e-' + places);
 }
 
+function resetCalculator() {
 
-function run() {
-    numbersButtons();
-    //operatorsButtons();
-    updateDisplay();
+    textInScreen = '0';
+    setDisplayResult(textInScreen);
+    firstNumber = null;
+    secondNumber = null;
+    operator = null;
+    unHighLightOperator();
 }
-
-run();
